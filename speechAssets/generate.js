312ff7b34a33ce983e5a1778b0d05b349e883471
Miguel Mota <mmota@ssheth-lt.msp.lan>
2016-01-27 11:53:25 -0800

    update utterances

--- speechAssets/generate.js
+++ speechAssets/generate.js
@@ -5,8 +5,8 @@ var intentUtteranceExpander = require('intent-utterance-expander');
 var intents = {
   ReadTopStoriesIntent: '(read|tell|what\'s|what are|get|fetch) top (|{Count}) (|stories|headlines|news)',
   ReadNewStoriesIntent: '(read|tell|what\'s|what are|get|fetch) (|latest) (|latest|new|newest|recent|most recent|current) (|{Count}) (|stories|headlines|news)',
-  ReadShowStoriesIntent: '(read|tell me|what\'s|what are|get|fetch) (|latest) (show|show h.n.|show hacker news|show and tell) (|{Count}) (|stories|headlines|news)',
-  ReadAskStoriesIntent: '(read|tell|what\'s|what are|get|fetch) (|latest) (ask|ask h.n.|ask hacker news|questions|answer) (|{Count}) (|stories|headlines|news)',
+  ReadShowStoriesIntent: '(read|tell me|what\'s|what are|get|fetch) (|latest) (show|show hacker news|show and tell) (|{Count}) (|stories|headlines|news)',
+  ReadAskStoriesIntent: '(read|tell|what\'s|what are|get|fetch) (|latest) (ask|ask hacker news|questions|answer) (|{Count}) (|stories|headlines|news)',
   ReadJobStoriesIntent: '(read|tell|what\'s|what are|get|fetch) (|latest) (job|jobs|career|careers|work) (|{Count}) (|stories|headlines|news)',
   ReadBestStoriesIntent: '(read|tell|what\'s|what are|get|fetch) (|latest) (best|bestest) (|{Count}) (|stories|headlines|news)',
   ReadActiveStoriesIntent: '(read|tell|what\'s|what are|get|fetch) (|latest) (active|most active) (|{Count}) (|stories|headlines|news)',
