5812c22adf1fbca95d9d76fb3f4a29107f610c73
Miguel Mota <miguelmota2@gmail.com>
2016-02-03 20:10:15 -0800

    update utternaces

--- speechAssets/generate.js
+++ speechAssets/generate.js
@@ -3,14 +3,39 @@
 var intentUtteranceExpander = require('intent-utterance-expander');
 
 var intents = {
-  ReadTopStoriesIntent: '(read|tell|what\'s|what are|get|fetch) top (|{Count}) (|stories|headlines|news)',
-  ReadNewStoriesIntent: '(read|tell|what\'s|what are|get|fetch) (|latest) (|latest|new|newest|recent|most recent|current) (|{Count}) (|stories|headlines|news)',
-  ReadShowStoriesIntent: '(read|tell me|what\'s|what are|get|fetch) (|latest) (show|show hacker news|show and tell) (|{Count}) (|stories|headlines|news)',
-  ReadAskStoriesIntent: '(read|tell|what\'s|what are|get|fetch) (|latest) (ask|ask hacker news|questions|answer) (|{Count}) (|stories|headlines|news)',
-  ReadJobStoriesIntent: '(read|tell|what\'s|what are|get|fetch) (|latest) (job|jobs|career|careers|work) (|{Count}) (|stories|headlines|news)',
-  ReadBestStoriesIntent: '(read|tell|what\'s|what are|get|fetch) (|latest) (best|bestest) (|{Count}) (|stories|headlines|news)',
-  ReadActiveStoriesIntent: '(read|tell|what\'s|what are|get|fetch) (|latest) (active|most active) (|{Count}) (|stories|headlines|news)',
-  ReadNoobStoriesIntent: '(read|tell|what\'s|what are|get|fetch) (|latest) noob (|{Count}) (|stories|headlines|news)'
+  ReadTopStoriesIntent: [
+    '(read|tell|what\'s|what are|get|get me|fetch) top (|{Count}) (|stories|headlines|news)',
+    'top (stories|headlines)'
+  ],
+  ReadNewStoriesIntent: [
+    '(read|tell|what\'s|what are|get|get me|fetch) (|latest) (|latest|new|newest|recent|most recent|current) (|{Count}) (|stories|headlines|news)',
+    'new (stories|headlines)',
+    'what the latest news is'
+  ],
+  ReadShowStoriesIntent: [
+    '(read|tell me|what\'s|what are|get|get me|fetch) (|latest) (show|show hacker news|show and tell) (|{Count}) (|stories|headlines|news)',
+    'show (stories|headlines)'
+  ],
+  ReadAskStoriesIntent: [
+    '(read|tell|what\'s|what are|get|get me|fetch) (|latest) (ask|ask hacker news|questions|answer) (|{Count}) (|stories|headlines|news)',
+    'ask (stories|headlines)'
+  ],
+  ReadJobStoriesIntent: [
+    '(read|tell|what\'s|what are|get|get me|fetch) (|latest) (job|jobs|career|careers|work) (|{Count}) (|stories|headlines|news)',
+    'job (stories|headlines)'
+  ],
+  ReadBestStoriesIntent: [
+    '(read|tell|what\'s|what are|get|get me|fetch) (|latest) (best|bestest) (|{Count}) (|stories|headlines|news)',
+    'best (stories|headlines)'
+  ],
+  ReadActiveStoriesIntent: [
+    '(read|tell|what\'s|what are|get|get me|fetch) (|latest) (active|most active) (|{Count}) (|stories|headlines|news)',
+    'active (stories|headlines)'
+  ],
+  ReadNoobStoriesIntent: [
+    '(read|tell|what\'s|what are|get|get me|fetch) (|latest) noob (|{Count}) (|stories|headlines|news)',
+    'active (stories|headlines)'
+  ]
 };
 
 var utterancesCollection = '';
